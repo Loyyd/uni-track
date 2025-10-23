5a21d3caccaa4f9849e136eb59288e746cc97f5c
Loyyd <konradkunkel@gmx.de>
2025-10-23 17:01:54 +0100

    /.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // Set `base` to your repository name for GitHub Pages project site
 export default defineConfig({
-  base: '/university_grade_tracker/',
+  base: './',
   plugins: [react()],
 })
